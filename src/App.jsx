1f8f8e32da60b78099230b7772ba9af5f7fb8504
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-23 17:07:28 -0300

    Finalizando formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Formulario from "./components/FormularioSimple"
 import './App.css'
 
 function App() {
 return (
         <>
-        <FormularioSimple/>
+        <div className="mainPage containerext">
+        <Formulario/>
+        </div>
         <footer className='bg-dark text-center text-secondary py-4'>
           <p>&copy; Todos los derechos reservados</p>
         </footer>
--- src/components/FormularioSimple.jsx
+++ src/components/FormularioSimple.jsx
@@ -1,49 +1,49 @@
 import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
-import "../App.css"
+import '../App.css';
 
-function Formulario(){
-    const [nombre, setNombre] = useState("")
-    const [apellido, setApellido] = useState("")
-    const [dni, setDni] = useState("")
-    const [email, setEmail] = useState("")
+function Formulario() {
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [dni, setDni] = useState('');
+  const [email, setEmail] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-const handleSubmit = (e) =>{
-e.preventDefault()
+    if (nombre && apellido && dni && email) {
+      alert('Datos enviados');
+    } else {
+      alert('Completar todos los datos');
+    }
+  };
 
-if (nombre && apellido && dni && email){
-    alert("Datos enviados")
-}else{
-    alert("Completar todos los datos")
- }
-}
-
-return(
-    <Container>
-        <h1 className="titulo">Formulario</h1>
-        <Form onSubmit={handleSubmit}>
-         <Form.Group controlId="nombre">
-            <Form.Label>Nombre: </Form.Label>
-            <Form.Control type="text" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
-         </Form.Group>
-         <Form.Group controlId="apellido">
-            <Form.Label>Apellido: </Form.Label>
-            <Form.Control type="text" value={apellido} onChange={(e) => setApellido(e.target.value)}/>
-         </Form.Group>
-         <Form.Group controlId="dni">
-            <Form.Label>D.N.I: </Form.Label>
-            <Form.Control type="text" value={dni} onChange={(e) => setDni(e.target.value)}/>
-         </Form.Group>
-         <Form.Group controlId="email">
-            <Form.Label>E-mail: </Form.Label>
-            <Form.Control type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-         </Form.Group>
-         <Button type="submit" variant="info"></Button>
-        </Form>
+  return (
+    <Container className="formcontainer">
+      <h1 className="titulo mb-4">Formulario</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="nombre">
+          <Form.Label className="fw-bold">Nombre:</Form.Label>
+          <Form.Control type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="apellido">
+          <Form.Label className="fw-bold">Apellido:</Form.Label>
+          <Form.Control type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="dni">
+          <Form.Label className="fw-bold">D.N.I:</Form.Label>
+          <Form.Control type="text" value={dni} onChange={(e) => setDni(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label className="fw-bold">E-mail:</Form.Label>
+          <Form.Control type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </Form.Group>
+        <Button className="mt-3"type="submit" variant="info">
+          Enviar
+        </Button>
+      </Form>
     </Container>
-)
-
+  );
 }
 
 export default Formulario
